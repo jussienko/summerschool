4ae62cc77a2376a2d51fea0179b7a5484f413f4f
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2018-07-04 15:09:09 +0300

    Fixed the evolve

--- demos/continuous-integration/core.c
+++ demos/continuous-integration/core.c
@@ -41,7 +41,7 @@ void evolve(field *curr, field *prev, double a, double dt)
         for (j = 1; j < curr->ny + 1; j++) {
             curr->data[i][j] = prev->data[i][j] + a * dt *
                                ((prev->data[i + 1][j] -
-                                 2.0 * prev->data[i][j] +
+                                 4.0 * prev->data[i][j] +
                                  prev->data[i - 1][j]) / dx2 +
                                 (prev->data[i][j + 1] -
                                  2.0 * prev->data[i][j] +
